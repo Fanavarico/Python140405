fb2e31afd95ee38646ae67b853ff2634f50977ad
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-09-14 14:52:59 +0330

    Delete Session 5 directory

--- Session 5/class/c2.py
+++
@@ -1,7 +0,0 @@
-# string methods
-# lower
-text = "i LOve PythON"
-print(text.lower())
-text.upper()
-text.swapcase()
-text.title()
--- Session 5/class/c3.py
+++
@@ -1,8 +0,0 @@
-# index -> shomare vagon haye qataran
-# name -> qatare
-name = "Amir"
-# A -> 0
-# m -> 1
-# i -> 2
-# r -> 3
-print(name[0: 2: 1])
