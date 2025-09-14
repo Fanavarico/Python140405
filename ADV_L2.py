12505cde31da2824996f6a31a7cb0bdb4bf2c793
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-09-14 14:53:47 +0330

    Create ADV_L2.py

---
+++ ADV_L2.py
@@ -0,0 +1,1462 @@
+"""
+Created on Mon Sep  1 20:17:47 2025
+
+@author: apm
+
+
+
+ADV_L1 ------------ jalase gozashte
+
+--> sohbat kardim
+
+Ensan (en) ------- Machine (binary 0,1)
+
+           | interface|
+           
+          programming language --> c++ , c , python , fortran , ....java , typescript,....
+          
+          
+          
+python --> entkehab krdim
+--> OOP (object oriented programming ), community balaee dare --> full of libraries
+all tools related to computers
+
+
+
+
+
+-------
+computer ---> python + IDE
+
+
+poythono balad bashe , IDE --> CODE RO MIZNI
+
+
+IDE --> Spyder , vscodde, cursre
+
+
+
+--> python 3.12 --> 3.18  
+
+package --> library --> package -_> V...
+
+package pandas 18 --> dependency --> numpy12
+
+pandas 17 --> numpy 11
+
+
+--> environement besaz --> [python, ide  --> package]
+
+
+
+python + ide
+===========
+|         |
+|computer |
+|         |
+=========== 
+
+
+
+
+===================
+| env1    |  env2  |
+|---------|---------
+|   env3 |   env4 |
+==================
+
+
+===============
+| python , env| 
+===============
+
+
+
+
+Anaconda --> berizi 
+environment --> create new environment [python dfelan version]
+hala spyder---> [version] --> mriizi
+
+
+
+spyder --> beri too --> spyder root (baraye kole laprab)
+spyder (name env) ----> roo oon click --> [windows]
+
+
+
+
+max--->
+anaconda --> selct envrioenment--> spyder ro run
+
+
+
+
+spyder --> ya harchizi
+
+ye ghesmat dare codeto mizani
+ye gehsmat yeseri etelaat ro neshon mide 
+console (terminal )--> natijeye runet ro nshon mide
+
+save mikonm --> spyder--> .spy ---> .py 
+--> .python ---> file save mikoni
+
+ham mitoni open, vs code , ....
+
+
+
+
+
+-----
+telegram --> amal kmikone
+code bzni hamin tamam
+
+
+
+
+Human ( En ) --->interface ---> (Binary)
+
+interface--> zaban
+
+zaban --> khdodesh yekseri loghato grammar dare
+
+fransavi , italian , spanish , ....
+
+
+python --> agha jan man fght kalamate khdoamo mishnasam
+
+agar cherto eprt inja benevisi oono ignore mikonm
+
+note, tozihi bdm , --> farsi ???
+
+
+ye khat --> # 
+chan khat --> '''
+
+"""
+
+
+# comment bezari
+
+'''
+chan khat
+mikham rozih bedam
+
+
+python--> mesle yek ensan az bala b paeen az chap b rast
+mikhone --> ketabkhon
+
+
+vaghty bzni rooye domeye run
+
+
+--telegram--> koli type koni
+ta zmani k  rooye send nzni chizi ersal nashod
+
+koi bnvis--> run njoni chizi ejra nmishe
+
+
+
+# ''' '''
+
+farsi , englishi , b zabane python hrf bzni
+python--> zbaane --> loghate , grammar
+
+ag gheyr az in harf bzni?0---> error midm ejaze ndre
+
+
+
+
+
+#-------------------------------
+b zabn python bnvisi joz # --> azadi
+
+------zabane python-------------
+#---------reserved words-----------
+1---> python built in function (tabe haye dakhelie python)
+2---> keywords (kilidvazhe ha)
+
+
+#--------unreserved words------------
+3--> variables (moteghayer ha)
+
+
+sakhtanesh --> k yek esme () mizni ye kari mikone --> [1]
+yejaee az codeto mikhay algorithemo avaz koni, az 
+bala b paen tak take khataro mikhoni , ---> keywords [2]
+
+har chizi ham k mikhay save kjoni --> zakhriash koni--> variable (moteghayer--> zarf
+                                                                  
+                                                                  
+                                                                  )
+
+
+
+
+
+
+
+'''
+
+
+#1---python built in function
+print()
+input()
+len()
+type()
+open()
+enumerate()
+zip()
+#.......
+
+
+#2---keywiords
+
+#....
+#...
+#...
+
+if 
+for
+while
+or and 
+#>........
+
+
+
+#---unreserved------
+#harchi joz python built in fucntion & keywords shoma benevisi
+
+#behsh agha in chie???
+#sefidesh mikone
+#3------
+ali
+#NameError: name 'ali' is not defined
+ali oilehvar
+open()
+print(' ')
+if
+ali 
+khanom
+agha
+
+#ina unreserved word --> nemishansam
+
+
+#--> chi dar nazar--> shoma mikhay
+#y zarfi sashte bashi ye chizi brizi toosh (variable )
+'''
+
+zarf-----> esm dare (   yechizi ham toosh)
+
+variable name ( value )
+
+
+
+
+zarfet esmesh chie = oon chizi k mikhay brizi tosh
+
+'''
+
+gol=10
+
+#zarf --> esme zarf gol 
+#10--> tooye in zarf
+
+
+
+#man shayad 100 ta zarf 
+# oon zarf -->
+
+'''
+ghanoon
+
+1--> bayad az character haye englisi estefade koni
+2---> nemitoni ba adad shoro koni esmeto
+3--> esm haye reserve shdoe ro nmitoni esme zarf bzni
+4---> nmitoni az hcih characteri joz _ estefade koni (fasele space NA)
+5- yadet bashe case sensetive --> [ali ALI]
+
+bejaye fasele --> _ bezar khoshgel 
+
+zarfe ali
+
+zarfe_ali
+ZarfeAli
+
+
+'''
+
+
+#zarf=
+
+#2zarf=
+
+#zar^=
+#zarf**=
+
+#do kalame
+
+#zarfe ali=10
+#chika konm????
+#2 ta rah
+#1 rah 
+zarfe_ali=10
+
+#2 rah -->
+ZarfeAli=10
+
+#case sensetive
+ali=10
+ALI=20
+
+#motefavetan
+
+#hamashon esme zarf --> chizaee k samte chapan
+#mikhay berizi dkahelesh --> raste zarf
+
+zarf=10
+
+'''
+[1]--->python built in function
+print() input() type() len() open() .....
+
+
+[2]---->keywords
+if else elif while for
+
+
+[3]-------------->variablkes
+yek zarfe esm dare, toosh chizi mirizan
+
+esmesh -_> nokatesho =
+chi mizarim tooshg?
+chia mitonim bzarim?--> type e mnoe moheme
+
+
+
+3.1. Numbers 
+        3.1.1. int 
+        3.1.2. float
+        3.1.3. complex
+       
+3.2. boolean (True,False)
+
+3.3. String --> character [harchizi k too keywordet mibini]
+
+
+3.4. Iterables
+
+
+
+'''
+
+
+#-----3.1.numbers---------------
+zarf=10
+zarf=10.2
+zarf=1j
+
+
+
+type(zarf) #Out[29]: complex
+
+
+#miad ychi msiaze b name zarf --> esmesho zwarf
+#adade int 10 ro mizare toosh
+
+zarf=10
+
+
+#zarf ro khali mikone , adade 20 ro mizare toosh
+zarf=20
+
+
+#nokte-->
+#calculators --_>
+
+#tavan , zarb , menha , taghsimo ,.....
+
+#a + b = c na nmishe
+
+a=10
+b=30
+
+#a + b = c --> ghalate
+
+#a + b --> 10 + 30 =40 --> koja brizam??--> to ye zarf --> c
+
+#yadet abaval bayad zarf ro bsazi
+
+
+
+c = a + b
+
+
+#bejaye + 
+
+
+'''
+olaviat
+() parantez
+** tavan
+* zarb
+/ taghsim
++ jam
+- tarigh
+
+
+'''
+
+
+
+d= a**b*c+2
+
+'''
+aval y zar banme d misaze
+--> a betavane b --> zarb c +2
+'''
+
+'''
+zarf =   amaliat 
+
+amaliat ** * + - /
+
+
+
+moghayese chi???
+aya mtionm mogahyese konm??
+
+
+
+==   aya mosavi hast>?
+!=  namosavi hast?
+>  aya bzoorgtr ast
+
+< aya kochaktar ast
+
+
+'''
+
+a=10
+b=20
+
+c= a + b
+
+
+#c = a > b
+
+
+
+a > b  # aya a bozorgtr az b hast??? 
+
+
+#soal -> python bht javab bde--> are ya na
+
+#ag nabashe -->  False
+
+#ag bashe --> True
+
+'''
+
+alaeme 
+
+==
+!=
+> 
+<
+
+
+dar ezash montazere \
+    soal porsidi -- .ajavb bde
+'''
+
+
+#dastor mdii
+
+#c ro bsaz
+#a + b kon
+#beriiiz toye c
+c= a + b
+
+
+
+#aya a > b hast???
+a >b
+
+
+
+#_---
+
+zarf1=100 #int
+
+
+zarf2=True #Bool --> Boolean
+
+
+#---------
+#----3-variables-----
+#---3.1. numbers (int, float, complex)
+#---3.2. Boolean ( True, False)
+#---3.3.
+
+
+
+
+
+
+a=20
+
+
+print(a)
+
+
+
+
+
+
+#print(z)
+#z=20
+
+#NameError: name 'z' is not defined
+
+
+a=40
+#dele python built in function (print,....)
+#delet keywords -->< nist
+#ahan --> unreserv-> bayad esme yek zarf bashe
+#montazwre = adad --> assign meghdardehi
+
+
+
+b=50
+
+print2=40
+
+
+
+print(b)
+
+type(b)
+
+#tabe haye dkaheli --> yekj kalaman () vorodi migire --> yeka kari mikonan
+
+
+#-----3----string--------
+
+#ali
+
+
+#NameError: name 'ali' is not defined
+
+ali
+
+# a
+# l
+# oi
+
+#[1]print, buitl in nist
+#[2]keyword
+#-->unreserv[3]--> esme zarf 
+#--> = hsdkjhsa
+
+
+#esme zarf migire
+
+
+#az chap 
+#z   a    r   f  =
+#zarf --> zarfe --> = 
+#Mikahd megdhar briz
+
+# a  l  i --> esme y zarfe dgas 
+
+#megdhari ik tooye zarf
+#zarf = chizi k tooye zarfe ali
+
+zarf=ali
+#NameError: name 'ali' is not defined
+
+#na aghaa
+#man zarfi bname ali ndrm k bkham rtohso i
+#alio zakhrie khode kalameye ali
+#agha man az koja bfhmm
+#to manzoret esme zarfe, ya ye kalame\
+    #y hrf
+    #jomle????
+    
+#---> ''  harchi tosh briiz
+
+#reshte haee az keyworddet has
+#Ino dg esme zarf dr nzr nmigire#
+#--> megdhar -->value
+
+
+'''
+
+zarf ---> 1-esme zarf 2-megdhare toosh
+
+variable --> 1-variable name 2-value
+
+
+
+
+varibale name / value --> ''
+'''
+
+
+zarf = 'ali'
+
+#zarf besaz -> esme zarf
+
+
+#zarf drm 
+#hana esme zarfed 
+
+#tooye zarf 'ali'
+
+hana = 'ali'
+
+
+
+print(ali)
+#NameError: name 'ali' is not defined
+
+
+#ali e brsm
+
+print(hana ) #toye dele hana on chiz k has
+
+
+
+
+
+
+print('ali')
+print(ali)
+
+
+#boto mohtaviate zarfe aliu ro print kon
+print(ali)
+#baayd aval zarfi bname ali bashe
+
+
+
+#megdhare alio print
+#keyword, rehste az
+print('ali')
+print("ali")
+
+
+#----
+
+#a , b ,c
+
+a=10
+#zarf --> esme zarf a
+#dakheel zarf -> 10
+
+type(a) #int
+
+c=a/2
+
+
+
+b='10'
+#zarf-->esmesh has b
+#dakhelesh has 10
+type(b) #str
+
+# keyworde 1 keyworde 0
+
+c=b/2
+#TypeError: unsupported operand type(s) for /: 'str' and 'int'
+
+
+
+#str --> string --> reshte -_> rehste hayee az keyworde laptabet
+
+
+#a = b 
+
+'''
+dastoori
+=
+**
+*
+/
++
+-
+
+'''
+
+'''
+amalgarhaye moghaye 
+==
+!=
+>
+>=
+<
+<=
+
+'''
+
+#dastor
+#aya a barabar hast ba b?
+
+#10
+#'10'
+a == b  #False
+
+type(a) #Out[59]: int
+a/10
+
+
+type(b) #Out[60]: str
+b/10
+
+
+a=' salam bar hameye azizan '
+
+a='alipilehvar@gmail.com'
+
+b='salam * * * * #'
+
+
+'''
+1----python built in function-->print() , iinput()
+2---keywords------if , else , for , while ,..
+3---->variable 
+    3.1.numebrs (int,float,compelx  , + - , == , !=)
+    3.2. bool (True, False)
+    3.3. Str ( ' ' ''  'ali' '20' ) 
+
+'''
+
+
+number=10
+#esme zarf number --> tosh 10
+
+print(number)
+
+#print() --> toosh ahrchi bzari
+
+#tooye ono 
+
+
+#miam samte rast namayehs mdim,
+
+
+
+print(ali)
+#NameError: name 'ali' is not defined
+
+
+#ali -_> y zarfe -->megdhari toshe
+#b oonmghda dastresi
+
+#zarfi bname ali ma nasakhtim
+
+
+
+#---> ag mikhay ychi bnvisi
+
+#man befhamam oon
+#esme zarfe ya yek megdhare
+
+#esme zarf
+
+zarf=
+
+
+#'ali'
+
+
+print('ali') #ali
+
+
+zarf='ali'
+
+#esme zarf --> zarf
+#ali 
+
+
+print(zarf)
+#toye zarf --> ali --> ali print mikone
+
+
+a=10
+#adad
+#yek adad hast
+
+#a+ - ...
+
+
+b='10'
+
+#b='10'
+#strin, reshte
+#+ - ....
+
+
+
+
+
+#numbers ( int, float, compelx)
+#True false bool
+#Str --> string --> reshte
+
+#zamani tooye ye zarfe dg
+#ychi brizi
+#k tashkl shdoe az hrchizi k too keyworde laptabete
+
+
+
+#238362762
+
+#shgyfjysHGHas
+#salam
+#ali
+#khsoh amadi
+#dkjgJDUKHk sax xhxzhjkHN
+
+#''
+
+
+zarf='salam khedmate hamegi'
+
+#zarf --> zarf
+
+#toosh 'salam kehdmate hamegi 
+
+
+#typ[e -> str]
+
+
+
+#=====================
+#=====================
+#-------------------
+#.3.3
+#=====================
+#----str ----------
+
+#----3.3.1.-------- Assign --> meghdar dehi
+
+#esme zarf = ' intoo mizari' 
+a='ali'
+
+#zarf esme zarf a
+#toosh --> ali
+
+
+print(a)
+
+#print( mohtaviate dakhele zarfi b name a)
+#ali
+
+
+
+
+#typoe ??
+a='ali'
+
+type(a) #str
+
+
+a='ali'
+
+len(a) #3
+
+
+
+#----change-----
+a='ali'
+#$a --> ali
+
+a='mohsen'
+#a --> mohsen
+
+
+
+#tabe --> narenji --> mokhtase hamas
+
+#zarfet adad, str , boolena ,.....
+#tabe haye kolie python hastannnn
+
+
+#[1] oython built in functions
+#print()  type() len()
+
+#----3.3.2.------- access (dastrsssi)
+
+zarf='alipilehvarmeibody'
+
+len(zarf) #Out[69]: 18
+
+print(len(zarf)) #18
+
+
+#18 ghorof dare
+
+
+
+'''
+
+-----memory-------
+|      name     |
+|        type     |
+|        len    |
+|      value
+
+
+ye khone
+koli khone darim b ezaye har horof
+
+alipilehavrmeibody
+
+
+
+[a]  [l]  [i]  [p]  [i]  [l]
+0     1    2    3    4
+
+sevomin harf
+joze dovom
+element dovom
+joze 2
+
+neshangare 2
+index 2
+
+
+
+'''
+
+
+#zarf darma bename name
+
+name='alipilehvarmeibody'
+
+
+
+#a l i p i l e hvar...
+#0 1 2 3 4 5 6 
+
+
+#farsi--> charomin harf
+#--><compiuter
+
+#kahneye chandom?  khaneye 3
+# indexe 3
+
+#b indexe 3 dastresi peyda konm
+
+
+#esmezarf[index]
+
+
+
+name     # 'alipilehvarmeibody'
+#charom--> indexe 3
+
+
+name[3] #'p'
+
+#avalin harf brsm?? -->indexe 0
+name[0] #'a'
+
+
+len(name) #18
+
+
+#0 1 2 3 4 .......... 17
+
+#len -1 
+
+name[17] #'y'
+
+
+
+#python --> -1
+
+#-1 --> 2 ta kole python
+#akhar
+#not found peyda nashod
+
+#ali pilehavr meibody
+
+#akhar --> 
+name[17]
+name[-1] #'y'
+
+
+name[16]
+name[-2]
+
+#a l i p i l e h a v r  m   e  i b  o  d  y
+#0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 
+#                               -4 -3 -2  -1 
+
+
+#yedone dastresI/
+#chanta
+
+'''
+name[0]='f'
+#TypeError: 'str' object does not support item assignment
+
+'''
+
+#----3.3.3-------- slicing
+#chanta mikham
+
+#alipilehvarmeibody
+
+#pilehavro mikham
+
+#az 4omin ta 11omin harf
+
+#--> programming
+
+# az indexe 3 mikham ta indexe 10
+
+#esmezarf[index] -->yedone mdiad
+
+
+#start index , end index
+
+
+#:--->ta 
+
+#hamishe toye python range --> akharie shamel nmishe ****8
+
+#esmezarf[start_index :end_index]
+
+
+#az indexe 3 ta 10 -->
+# 3 4 5 6 7 8 9 10 XXXX11
+
+name #'alipilehvarmeibody'
+
+name[3] #'p'
+
+#az 3 bde ta 11(11 shamel nmishe)
+#az 3 ta 10
+name[3:11] #'pilehvar'
+
+
+
+name[4:8]
+
+name[5:9]
+
+
+#-----
+#belfarz
+#zarf[start_index:end_index]
+
+#start_index nazari--> 0
+#end_index--> akahr
+
+
+#a l i p i l
+#0 1 2 3 4 5 
+
+#0 1 2 
+
+name[0:3] # 'ali'
+name[:3] #'ali'
+
+name[14:18] # 'body'
+name[14:]
+
+
+
+#a l i p i l e h v a r
+#0 1 2 3 4 5  6
+
+#l i p i
+name[1:5]  #
+
+name[1:5:1] #same
+
+
+name[1:5:2]
+
+
+#esme_zarf[start_index:end_index]
+
+
+##esme_zarf[start_index:end_index:step]
+
+
+
+#-----3.3.4 ---> str functions ....
+
+#-->flashback
+#python buolt in function
+#fucntion dkaheli
+#function --> ()
+
+#tabe haye dkaheli python
+#rangi mishe, baraye hameye variable ha has
+#str , adad
+
+print(10)
+print(10.56)
+print('salam')
+print(True)
+
+print()
+input()
+len()
+type()
+type('ali')
+type(10)
+
+
+#--str functions -->
+#farghe aval --> in ha --> faghato fght baraye str ha hastan
+#na baray ehame
+
+#ye tabe --> lower() kochik mikone zzarfe
+#lower()
+#upper()
+#title()
+#find()
+#coutn()
+
+
+name='ALI'
+
+
+print(name) #ALI
+
+
+len(name)
+
+#kochikesh konm
+
+#lower
+#chon fucntion
+
+#lower()
+
+lower(name)
+#NameError: name 'lower' is not defined
+
+
+#esme tabe(zarf) -->nemishe
+
+
+#esme_zarf.esme_tabe()
+
+name.lower() #'ali'
+
+
+print(name)  #ALI
+
+
+#--> in tabe ha (farghe3)-->in tabe ha emal nmishan , khrooji midan
+
+
+'''
+tabe ha
+
+-> emal mikonan
+-->khroojimidan (emal nmikonan)
+
+
+
+emal mikonan -_> fabaha --> aslie taghir kone
+
+khrooji midan --> aslie to taghir nmikone
+(bayad y zarfe jadi bsazi bzari jolo tabe )
+'''
+
+
+
+name='ALI'
+
+print(name) #ALI
+
+
+#name.lower() --> ali
+
+
+edited_name=name.lower()
+
+print(name) #ALI
+
+print(edited_name) #ali
+
+
+
+'''
+lower(), upper() , title() , coutn() , find() ,....
+
+str functions--------
+1--> built in fucntion->ina baraye ham enisan , fgth baraye zarfe str
+2--> lower(zarf) --> zarf.lower()
+3--> ina emal nmishan khroji midan
+#khode zarf taghir nmikone
+bayad zarf jadid jolosh bzari--> taghri
+
+zarf_jadid=zarf.lower()
+
+
+
+
+zarfe_jadid=zarf.function()
+
+
+#function() just for str
+'''
+
+
+'''
+
+str function() 
+
+-------str fucntion (1)
+kareshon convert --> tabdil
+
+lower() --> kochk mikonad
+upper() -->bozogr miokone
+title() --> 
+
+'''
+
+name='in the name of god'
+
+new_name=name.title()
+
+print(name) #'in the name of god'
+print(new_name) #In The Name Of God
+
+
+#---->
+name.upper() #Out[104]: 'IN THE NAME OF GOD'
+
+
+name.capitalize()
+#Out[105]: 'In the name of god'
+
+
+
+
+
+'''
+--------
+str fucntion number
+
+adad
+
+.count()
+
+.find()
+
+
+'''
+#a ro bshmori 
+
+#name.count(a)
+
+
+#in the name of god
+
+name.count('a') #1
+
+name.find('a') #8
+
+
+'''
+----- True false
+
+
+is
+
+
+
+
+'''
+
+name.islower() #True
+
+name.isupper()
+
+
+
+'''
+liste kamel ro inaj mizaram
+
+
+#ref : https://www.w3schools.com/python/python_ref_string.asp
+
+Method	       Description
+capitalize()	Converts the first character to upper case
+
+casefold()	Converts string into lower case
+
+center()	Returns a centered string
+
+count()	Returns the number of times a specified value occurs in a string
+
+encode()	Returns an encoded version of the string
+
+endswith()	Returns true if the string ends with the specified value
+
+expandtabs()	Sets the tab size of the string
+
+find()	Searches the string for a specified value and returns the position of where it was found
+
+format()	Formats specified values in a string
+
+format_map()	Formats specified values from a dictionary in a string
+
+
+index()	Searches the string for a specified value and returns the position of where it was found
+
+isalnum()	Returns True if all characters in the string are alphanumeric
+
+isalpha()	Returns True if all characters in the string are in the alphabet
+
+isascii()	Returns True if all characters in the string are ascii characters
+
+isdecimal()	Returns True if all characters in the string are decimals
+
+isdigit()	Returns True if all characters in the string are digits
+
+isidentifier()	Returns True if the string is an identifier
+
+islower()	Returns True if all characters in the string are lower case
+
+isnumeric()	Returns True if all characters in the string are numeric
+
+isprintable()	Returns True if all characters in the string are printable
+
+isspace()	Returns True if all characters in the string are whitespaces
+
+istitle()	Returns True if the string follows the rules of a title
+
+isupper()	Returns True if all characters in the string are upper case
+
+join()	Converts the elements of an iterable into a string
+
+ljust()	Returns a left justified version of the string
+
+lower()	Converts a string into lower case
+
+lstrip()	Returns a left trim version of the string
+
+maketrans()	Returns a translation table to be used in translations
+
+partition()	Returns a tuple where the string is parted into three parts
+
+replace()	Returns a string where a specified value is replaced with a specified value
+
+rfind()	Searches the string for a specified value and returns the last position of where it was found
+
+rindex()	Searches the string for a specified value and returns the last position of where it was found
+
+rjust()	Returns a right justified version of the string
+
+rpartition()	Returns a tuple where the string is parted into three parts
+
+rsplit()	Splits the string at the specified separator, and returns a list
+
+rstrip()	Returns a right trim version of the string
+
+split()	Splits the string at the specified separator, and returns a list
+
+splitlines()	Splits the string at line breaks and returns a list
+
+startswith()	Returns true if the string starts with the specified value
+
+strip()	Returns a trimmed version of the string
+
+swapcase()	Swaps cases, lower case becomes upper case and vice versa
+
+title()	Converts the first character of each word to upper case
+
+translate()	Returns a translated string
+
+upper()	Converts a string into upper case
+
+zfill()	Fills the string with a specified number of 0 values at the beginning
+
+
+'''
+
+#-----example-------
+#mosheklio baraye jhalasye ayande
+#mitonid ono hal koni
+
+'''
+AMAZON --> DIGIKALA BARNAM EMINEVISIM
+
+FOROSHANDE HASH ...............
+
+yek barname ee benevisid k biad
+az moshtari  name mahsool begire beriae to y zarf
+code mahsolo begire
+GHEYAMTE MAHSOOLO begire
+
+tahesh bsh kh khoshgel print
+yeja print kone
+
+=========
+ sherkate plutus====
+ 
+ mahsoL : \
+     
+    code:
+    gheymat:
+        
+        
+    aya taeed mikonid?
+    
+
+
+yes --> benevise tabrik migam
+no --> motasefam
+
+
+
+
+
+#---> part 1
+
+
+
+#--->part2 -->
+Yes 
+YES
+ yes
+ 
+--> tabrik migam
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
